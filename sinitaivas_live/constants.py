23739505c0c75a291baf0af473404b52f58ed9b8
letizia iannucci <letizia.iannucci@aalto.fi>
2025-06-11 17:19:43 +0300

    better typing

--- sinitaivas_live/constants.py
+++ sinitaivas_live/constants.py
@@ -1,6 +1,7 @@
 import utils.files_storage as fs
+from typing import Final
 
 
-CURSORS_FILE = "cursors.json"
+CURSORS_FILE: Final = "cursors.json"
 
-PATH_TO_CURSORS_FILE = f"{fs.current_dir()}/{CURSORS_FILE}"
+PATH_TO_CURSORS_FILE: Final = f"{fs.current_dir()}/{CURSORS_FILE}"
