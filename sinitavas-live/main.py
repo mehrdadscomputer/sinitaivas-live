b2c900763470f5f45e7495975d6d05a1a2ba165f
letiziaia <45148109+letiziaia@users.noreply.github.com>
2025-06-11 13:10:59 +0300

    Create main.py

---
+++ sinitavas-live/main.py
@@ -0,0 +1,33 @@
+import click
+from firehose.streamer import streamer_main
+from utils.logging import logger, handle_catch_error
+
+
+@handle_catch_error
+@click.command()
+@click.option(
+    "--mode", default="fresh", help="Mode to run the streamer [fresh/resume]."
+)
+def main(mode: str):
+    """
+    Main function to run the streamer.
+
+    Parameters:
+        mode (str): Mode to run the streamer [fresh/resume].
+    Returns:
+        None
+
+    ----------------
+    Example usage:
+    ----------------
+    python -m firehose.main --mode fresh
+    python -m firehose.main --mode resume
+    """
+    logger.info(f"Starting streamer process as {mode}")
+    if mode not in ["fresh", "resume"]:
+        raise ValueError("Only fresh and resume modes are supported")
+    streamer_main(mode)
+
+
+if __name__ == "__main__":
+    main()
